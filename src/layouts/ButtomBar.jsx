000ef09290a3660215e9df479d509b210f7ca805
Sophon-Kr <77550438+Sophon-Kr@users.noreply.github.com>
2021-07-30 12:10:37 +0700

    create buttom tab room

--- src/layouts/ButtomBar.jsx
+++ src/layouts/ButtomBar.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import SwipeableViews from 'react-swipeable-views';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-
+import React from "react";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
 
@@ -15,8 +14,8 @@ function TabPanel(props) {
     <div
       role="tabpanel"
       hidden={value !== index}
-      id={`full-width-tabpanel-${index}`}
-      aria-labelledby={`full-width-tab-${index}`}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
       {...other}
     >
       {value === index && (
@@ -36,62 +35,62 @@ TabPanel.propTypes = {
 
 function a11yProps(index) {
   return {
-    id: `full-width-tab-${index}`,
-    'aria-controls': `full-width-tabpanel-${index}`,
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
   };
 }
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    // flex: "dislay",
     backgroundColor: theme.palette.background.paper,
-    width: 500,
+  },
+  title: {
+    flexGrow: 1,
+    justifyItems: "center",
   },
 }));
 
-export default function FullWidthTabs() {
+export default function ButtomBar() {
   const classes = useStyles();
-  const theme = useTheme();
   const [value, setValue] = React.useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
-  const handleChangeIndex = (index) => {
-    setValue(index);
-  };
-
   return (
     <div className={classes.root}>
-      <AppBar position="static" color="default">
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          indicatorColor="primary"
-          textColor="primary"
-          variant="fullWidth"
-          aria-label="full width tabs example"
+      <TabPanel value={value} index={0}>
+        Room2001
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Room2056
+      </TabPanel>
+      <Grid container spacing={1}>
+        <AppBar
+          position="fixed"
+          style={{
+            backgroundColor: "#FFFFFF",
+            color: "#2D62ED",
+            top: "auto",
+            bottom: 0,
+          }}
         >
-          <Tab label="Item One" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} />
-        </Tabs>
-      </AppBar>
-      <SwipeableViews
-        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-        index={value}
-        onChangeIndex={handleChangeIndex}
-      >
-        <TabPanel value={value} index={0} dir={theme.direction}>
-          Item One
-        </TabPanel>
-        <TabPanel value={value} index={1} dir={theme.direction}>
-          Item Two
-        </TabPanel>
-        <TabPanel value={value} index={2} dir={theme.direction}>
-          Item Three
-        </TabPanel>
-      </SwipeableViews>
+          <Grid container>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              aria-label="simple tabs example"
+              indicatorColor="primary"
+              centered
+            >
+              <Tab label="Room2011" {...a11yProps(0)} />
+              <Tab label="Room2056" {...a11yProps(1)} />
+            </Tabs>
+          </Grid>
+        </AppBar>
+      </Grid>
     </div>
   );
 }
