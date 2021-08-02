dd14a503ee801ab979c7387a7500a128434f2cd8
Sophon-Kr <77550438+Sophon-Kr@users.noreply.github.com>
2021-08-02 18:19:09 +0700

    add layout detail in dashboard

--- src/App.js
+++ src/App.js
@@ -34,14 +34,14 @@ function App() {
   // }
   return (
     <Provider store={store}>
-      <div >
+      <div>
         <BrowserRouter>
           <Header />
           <Switch>
             <Route exact path="/signin" component={SignIn} />
             <Route exact path="/forgotpassword" component={ForgotPass} />
             <Route exact path="/userlist" component={UserList} />
-            
+
             <Route exact path="/" component={Dashboard} />
             {/* <Route component={ErrorPage} /> */}
           </Switch>
