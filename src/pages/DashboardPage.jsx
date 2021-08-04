c4ecf6fd3a55128041404997f6cf3e0c685d13b5
Sophon-Kr <77550438+Sophon-Kr@users.noreply.github.com>
2021-08-04 11:28:45 +0700

    berfore merge

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -37,18 +37,8 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 
 // import { useDispatch } from 'react-redux/lib/hooks/useDispatch';
-<<<<<<< HEAD
 import { EDIT_LANG } from "../middleware/action";
 import { ReactReduxContext } from "react-redux";
-=======
-import {
-    EDIT_LANG
-} from "../middleware/action";
-import {
-    EDIT_COLOR
-} from "../middleware/action";
-import { ReactReduxContext } from 'react-redux'
->>>>>>> a2c6898297578ffdd503acf8c017ef32dfd023cf
 import ButtomBar from "../layouts/ButtomBar";
 import HeaderTabs from "../layouts/HeaderTabs";
 
@@ -59,112 +49,8 @@ import secondaryListItems_th from "../middleware/listitems/dropDownItems";
 // import { mainListItems_en, secondaryListItems_en } from '../middleware/listitems/dropDownItems';
 // import { mainListItems_th, secondaryListItems_th } from '../middleware/listitems/dropDownItems';
 
-<<<<<<< HEAD
 import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
 import RightBar from "../layouts/RightBar";
-=======
-const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-    },
-    toolbar: {
-        paddingRight: 24, // keep right padding when drawer closed
-    },
-    toolbarIcon: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'flex-end',
-        padding: '0 8px',
-        ...theme.mixins.toolbar,
-    },
-    appBar: {
-        zIndex: theme.zIndex.drawer +1,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-    },
-    themeDefault: {
-        backgroundColor: '#2D62ED'
-    },
-    themeGreen: {
-        backgroundColor: green[600]
-    },
-    themePurple: {
-        backgroundColor: purple[600]
-    },
-    themeFontDefault: {
-        color: '#2D62ED'
-    },
-    themeFontGreen: {
-        color: 'green'
-    },
-    themeFonrPurple: {
-        color: 'purple'
-    },
-    appBarShift: {
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-
-    },
-    menuButtonExpand: {
-         paddingRight: 20,
-         paddingLeft:40
-    },
-    logoExpand: {
-        marginLeft: -10,
-   },
-    menuButtonHidden: {
-        display: 'none',
-    },
-    drawerPaper: {
-        position: 'relative',
-        whiteSpace: 'nowrap',
-        width: drawerWidth,
-        transition: theme.transitions.create('width', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-        // backgroundColor: '#2D62ED',
-        color: 'white'
-    },
-    drawerPaperClose: {
-        overflowX: 'hidden',
-        transition: theme.transitions.create('width', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-        width: theme.spacing(7),
-        [theme.breakpoints.up('sm')]: {
-            width: theme.spacing(8.5),
-        },
-    },
-    appBarSpacer: theme.mixins.toolbar,
-    content: {
-        flexGrow: 1,
-        height: '100vh',
-        overflow: 'auto',
-    },
-    container: {
-        paddingTop: theme.spacing(0),
-        paddingBottom: theme.spacing(0),
-        paddingLeft: theme.spacing(0),
-        paddingRight: theme.spacing(0),
-    },
-    paper: {
-        padding: theme.spacing(2),
-        display: 'flex',
-        overflow: 'auto',
-        flexDirection: 'column',
-    },
-    fixedHeight: {
-        height: 240,
-    },
->>>>>>> a2c6898297578ffdd503acf8c017ef32dfd023cf
 
 const drawerWidth = 240;
 
@@ -330,7 +216,6 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function Dashboard() {
-<<<<<<< HEAD
   const classes = useStyles();
   const items = [
     { name: "Dashboard", label: "Dashboard" },
@@ -460,93 +345,6 @@ export default function Dashboard() {
           aria-controls="primary-search-account-menu"
           aria-haspopup="true"
           color="inherit"
-=======
-    const classes = useStyles();
-    
-
-    const [open, setOpen] = React.useState(false);
-    const [themeState, setThemeState] = React.useState(classes.themeDefault);
-    const [themeFontState, setThemeFontState] = React.useState(classes.themeFontDefault);
-    const handleDrawerOpen = () => {
-        setOpen(true);
-    };
-    const handleDrawerClose = () => {
-        setOpen(false);
-    };
-    const { store } = useContext(ReactReduxContext)
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
-
-    const isMenuOpen = Boolean(anchorEl);
-    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
-    const handlelanguageMenuOpen = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-    const handleProfileMenuOpen = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    const handleMobileMenuClose = () => {
-        setMobileMoreAnchorEl(null);
-    };
-
-    const handleMenuClose = () => {
-        setAnchorEl(null);
-        handleMobileMenuClose();
-    };
-
-    const handleMobileMenuOpen = (event) => {
-        setMobileMoreAnchorEl(event.currentTarget);
-    };
-
-
-    const handleLanguage = (lang) => {
-        console.log("handle lang", lang)
-        // setAnchorEl(null);
-        // handleMobileMenuClose();
-        if (lang == 'th') {
-            setThemeState(classes.themePurple)
-            setThemeFontState(classes.themeFonrPurple)
-            store.dispatch({
-                type: EDIT_COLOR,
-                payload: purple[600]
-            })
-        } else {
-            setThemeState(classes.themeGreen)
-            setThemeFontState(classes.themeFontGreen)
-            store.dispatch({
-                type: EDIT_COLOR,
-                payload: green[600]
-            })
-        }
-        console.log("store", store)
-        console.log("store", store.store)
-        store.dispatch({
-            type: EDIT_LANG,
-            payload: lang
-        })
-
-        console.log("store", store.getState().reducer.lang)
-        setAnchorEl(null);
-        handleMobileMenuClose();
-    };
-
-
-
-
-
-    const menuId = "primary-search-account-menu";
-    const renderMenu = (
-        <Menu
-            anchorEl={anchorEl}
-            anchorOrigin={{ vertical: "top", horizontal: "right" }}
-            id={menuId}
-            keepMounted
-            transformOrigin={{ vertical: "top", horizontal: "right" }}
-            open={isMenuOpen}
-            onClose={handleMenuClose}
->>>>>>> a2c6898297578ffdd503acf8c017ef32dfd023cf
         >
           <AccountCircle />
         </IconButton>
@@ -689,7 +487,6 @@ export default function Dashboard() {
                   open={rightBar}
                   onClose={toggleRightBar(false)}
                 >
-<<<<<<< HEAD
                   {rightBarMenu()}
                 </SwipeableDrawer>
               </Grid>
@@ -708,117 +505,10 @@ export default function Dashboard() {
             </IconButton>
           </div>
           {/* <IconButton color="inherit">
-=======
-                    <ExplicitIcon />
-                </IconButton>
-                <p>Language</p>
-            </MenuItem>
-        </Menu>
-    );
-    const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
-
-    return (
-        <div className={classes.root}>
-            <CssBaseline />
-            <AppBar position="absolute" className={clsx(themeState, classes.appBar, open && classes.appBarShift)}>
-                <Toolbar className={classes.toolbar}>
-                    <img
-                        src="logomin.png"
-                        className={clsx(classes.logoExpand, open && classes.menuButtonHidden)}
-                        alt="..."
-                        height={35}
-                    />
-
-                    <IconButton
-                        edge="start"
-                        color="inherit"
-                        aria-label="open drawer"
-                        onClick={handleDrawerOpen}
-                        className={clsx(classes.menuButtonExpand, open && classes.menuButtonHidden)}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography className={classes.title} variant="h6" noWrap>
-                        RVS App Platform
-                    </Typography>
-                    <div className={classes.search}>
-                        <div className={classes.searchIcon}>
-                            <SearchIcon />
-                        </div>
-                        <InputBase
-                            placeholder=""
-                            classes={{
-                                root: classes.inputRoot,
-                                input: classes.inputInput,
-                            }}
-                            inputProps={{ "aria-label": "search" }}
-                        />
-                    </div>
-                    <div className={classes.grow} />
-                    <div className={classes.sectionDesktop}>
-                        <Fab
-                            size="small"
-                            aria-label="add"
-                            style={{ backgroundColor: "#64CFFF", color: "white" }}
-                        >
-                            <AddIcon />
-                        </Fab>
-                        <Divider
-                            orientation="vertical"
-                            flexItem
-                            style={{ backgroundColor: "#FFFFFF" }}
-                            variant="middle"
-                        />
-                        <IconButton
-                            edge="end"
-                            aria-label="account of current user"
-                            aria-controls={menuId}
-                            aria-haspopup="true"
-                            onClick={handlelanguageMenuOpen}
-                            size="medium"
-                            style={{ color: "#1B47C1" }}
-                        >
-                            <ExplicitIcon />
-                        </IconButton>
-                        <IconButton
-                            edge="end"
-                            aria-label="account of current user"
-                            aria-controls={menuId}
-                            aria-haspopup="true"
-                            onClick={handlelanguageMenuOpen}
-                            size="large"
-                            style={{ color: "#1B47C1" }}
-                        >
-                            <AccountCircle style={{size:50}} />
-                        </IconButton>
-                        <Grid item spacing={1} style={{ paddingLeft: 20 }}>
-                            <Grid item spacing={1}>
-                                <Typography variant="subtitle1" style={{fontSize:15}}>{store.getState().reducer.username}</Typography>
-                            </Grid>
-                            <Grid item spacing={1}>
-                                <Typography variant="body2" style={{fontSize:10}} >Admin</Typography>
-                            </Grid>
-                        </Grid>
-                    </div>
-
-                    <div className={classes.sectionMobile}>
-                        <IconButton
-                            aria-label="show more"
-                            aria-controls={mobileMenuId}
-                            aria-haspopup="true"
-                            onClick={handleMobileMenuOpen}
-                            color="inherit"
-                        >
-                            <MoreIcon />
-                        </IconButton>
-                    </div>
-                    {/* <IconButton color="inherit">
->>>>>>> a2c6898297578ffdd503acf8c017ef32dfd023cf
                         <Badge badgeContent={4} color="secondary">
                             <NotificationsIcon />
                         </Badge>
                     </IconButton> */}
-<<<<<<< HEAD
         </Toolbar>
       </AppBar>
       {renderMobileMenu}
@@ -855,51 +545,12 @@ export default function Dashboard() {
         </Grid>
         <Divider />
         {/* <List disablePadding dense>
-=======
-                </Toolbar>
-            </AppBar>
-            {renderMobileMenu}
-            {renderMenu}
-            <Drawer
-                variant="permanent"
-                classes={{
-                    paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose,themeState),
-                }}
-                open={open}
-            >
-                <Grid container >
-                    <Grid item container direction="row">
-                            <div style={{paddingLeft:12, paddingRight:10, paddingTop:18 }}>
-                                <img
-                                src="logo.png"
-                                class="rounded mx-auto d-block"
-                                alt="..."
-                                height={35}
-                                />
-                            </div>
-                            {open?
-                            <IconButton
-                                edge="start"
-                                className={classes.menuButton}
-                                color="inherit"
-                                aria-label="open drawer"
-                                onClick={handleDrawerClose}
-                            >
-                                <MenuIcon />
-
-                            </IconButton>: null }
-                    </Grid>
-                </Grid>
-                <Divider />
-                {/* <List disablePadding dense>
->>>>>>> a2c6898297578ffdd503acf8c017ef32dfd023cf
                     {items.map(({ label, name, ...rest }) => (
                         <ListItem key={name} button {...rest}>
                             <ListItemText>{label}</ListItemText>
                         </ListItem>
                     ))}
                 </List> */}
-<<<<<<< HEAD
         <List>
           {store.getState().reducer.lang == "en" ? (
             <MainListItems_en />
@@ -922,21 +573,3 @@ export default function Dashboard() {
     </div>
   );
 }
-=======
-                <List >{store.getState().reducer.lang == "en"? <MainListItems_en/>:<MainListItems_en/>}</List>
-                <Divider />
-                {/* <List>{store.getState().reducer.lang == "en"? <MainListItems_en/>:<MainListItems_en/>}</List> */}
-            </Drawer>
-            <main className={classes.content}>
-                <div className={classes.appBarSpacer} />
-                <Container maxWidth="100" className={classes.container}>
-                    {/* <FrontDesk /> */}
-                    <HeaderTabs />
-                    <div style={{paddingTop:50}}></div>
-                    <ButtomBar />
-                </Container>
-            </main>
-        </div>
-    );
-}
->>>>>>> a2c6898297578ffdd503acf8c017ef32dfd023cf
